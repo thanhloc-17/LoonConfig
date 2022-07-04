7ee5b92cb9b2eb024e48a1bbf65f4edec51afca6
Kikyou17 <ltloc17@icloud.com>
2022-07-04 10:55:14 +0700

    add api to leaderboard

--- script/revenuecat.js
+++ script/revenuecat.js
@@ -1,21 +1,42 @@
-let obj = JSON.parse($response.body);
-obj["subscriber"]["subscriptions"]= {
-      "revenuecat.pro.yearly": {
-        "is_sandbox": false,
-        "period_type": "active",
-        "billing_issues_detected_at": null,
-        "unsubscribe_detected_at": null,
-        "expires_date": "2099-12-01T03:51:32Z",
-        "original_purchase_date": "2019-10-31T02:51:33Z",
-        "purchase_date": "2019-10-31T02:51:32Z",
-        "store": "app_store"
-      }
-    };
-obj["subscriber"]["entitlements"]= {
-      "pro": {
-        "expires_date": "2099-12-01T03:51:32Z",
-        "product_identifier": "revenuecat.pro.yearly",
-        "purchase_date": "2019-10-31T02:51:32Z"
-      }
-    };
-$done({body: JSON.stringify(obj)});
+const resp = {};
+const obj = JSON.parse(typeof $response != "undefined" && $response.body || null);
+const ua = $request.headers['User-Agent'] || $request.headers['user-agent'];
+const list = {
+	'VSCO': { name: 'membership', id: 'com.circles.fin.premium.yearly' },
+	'Filebox': { name: 'filebox_pro', id: 'com.premium.yearly' },
+	'Fileball': { name: 'filebox_pro', id: 'com.premium.yearly' },
+	'1Blocker': { name: 'premium', id: 'blocker.ios.subscription.yearly' },
+  'Pro': {name: 'pro', id: 'revenuecat.pro.yearly'}
+};
+
+const data = {
+	"expires_date": "2030-02-18T07:52:54Z",
+	"original_purchase_date": "2020-02-11T07:52:55Z",
+  "product_identifier": "revenuecat.pro.yearly",
+	"purchase_date": "2020-02-11T07:52:54Z",
+  "store": "app_store",
+  "is_sandbox": false,
+  "period_type": "active",
+  "billing_issues_detected_at": null,
+  "unsubscribe_detected_at": null
+};
+
+if (typeof $response == "undefined") {
+	delete $request.headers["x-revenuecat-etag"]; // prevent 304 issues
+	delete $request.headers["X-RevenueCat-ETag"];
+	resp.headers = $request.headers;
+} else if (obj && obj.subscriber) {
+	obj.subscriber.subscriptions = obj.subscriber.subscriptions || {};
+	obj.subscriber.entitlement = obj.subscriber.entitlement || {};
+	for (const i in list) {
+		if (new RegExp(`^${i}`, `i`).test(ua)) {
+			obj.subscriber.subscriptions[list[i].id] = data;
+			obj.subscriber.entitlements[list[i].name] = JSON.parse(JSON.stringify(data));
+			obj.subscriber.entitlements[list[i].name].product_identifier = list[i].id;
+			break;
+		}
+	}
+	resp.body = JSON.stringify(obj);
+}
+
+$done(resp);
